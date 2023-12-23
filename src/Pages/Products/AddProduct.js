7b1f48c978978c204e2a1705d67a61c013b23e34
ahmed-tahoon <77838959+ahmed-tahoon@users.noreply.github.com>
2023-12-23 12:50:43 +0200

    Update AddProduct.js

--- src/Pages/Products/AddProduct.js
+++ src/Pages/Products/AddProduct.js
@@ -131,7 +131,7 @@ const AddProduct = ({ productList, setProductList }) => {
     <div className="container mx-auto p-4 min-h-screen">
       {error && (
         <div
-          class="flex items-center p-4 mb-4 text-sm text-red-800 border border-red-300 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 dark:border-red-800"
+          class="flex items-center  p-4 mb-4 text-sm text-red-800 border border-red-300 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 dark:border-red-800"
           role="alert"
         >
           <svg
@@ -143,7 +143,7 @@ const AddProduct = ({ productList, setProductList }) => {
           >
             <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
           </svg>
-          <div>{error}</div>
+          <div>Invalid_Input {error}</div>
         </div>
       )}
       <div className="flex justify-between items-center mb-4 bg-gray-200 p-4 rounded-md">
