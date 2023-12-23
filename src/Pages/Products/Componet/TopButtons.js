464261e3b7848a7382c061d09927aecd26852ef9
ahmed-tahoon <77838959+ahmed-tahoon@users.noreply.github.com>
2023-12-23 12:12:18 +0200

    Update TopButtons.js

--- src/Pages/Products/Componet/TopButtons.js
+++ src/Pages/Products/Componet/TopButtons.js
@@ -1,31 +1,41 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-
-const TopButtons = ({selectedProducts,handleDeleteSelected }) => {
-  const nav = useNavigate()
+const TopButtons = ({ selectedProducts, handleDeleteSelected }) => {
+  const nav = useNavigate();
 
   const goToAddProduct = () => {
     nav("/add-product");
   };
   return (
     <React.Fragment>
-     <div className="flex justify-between items-center mb-4 bg-gray-200 p-4 rounded-md">
-          <div className="flex-grow" />
-          <div className="flex items-center space-x-4">
-            <button onClick={goToAddProduct} className="bg-gradient-to-r from-green-500 to-green-700 text-white font-bold py-2 px-4 rounded-md hover:from-green-700 hover:to-green-900">
-              ADD
+      <div className="flex justify-between items-center mb-4 bg-gray-200 p-4 rounded-md">
+        <div className="flex-grow" />
+        <div className="flex items-center space-x-4">
+          <button
+            onClick={goToAddProduct}
+            className="bg-gradient-to-r from-green-500 to-green-700 text-white font-bold py-2 px-4 rounded-md hover:from-green-700 hover:to-green-900"
+          >
+            ADD
+          </button>
+          {selectedProducts.length > 0 && (
+            <button
+              onClick={handleDeleteSelected}
+              className="bg-gradient-to-r from-red-500 to-red-700 text-white font-bold py-2 px-4 rounded-md hover:from-red-700 hover:to-red-900"
+            >
+              MASS DELETE
             </button>
-            {selectedProducts.length > 0 && (
-              <button
-                onClick={handleDeleteSelected}
-                className="bg-gradient-to-r from-red-500 to-red-700 text-white font-bold py-2 px-4 rounded-md hover:from-red-700 hover:to-red-900"
-              >
-                Mass Delete ({selectedProducts.length})
-              </button>
-            )}
-          </div>
+          )}
+          {selectedProducts.length > 0 && (
+            <div className="flex items-center">
+              <span className="text-sm text-gray-500 mr-1">Selected:</span>
+              <span className="text-sm font-semibold text-red-500">
+                {selectedProducts.length}
+              </span>
+            </div>
+          )}
         </div>
+      </div>
     </React.Fragment>
   );
 };
