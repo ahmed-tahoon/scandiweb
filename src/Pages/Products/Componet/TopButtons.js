d786f7220ec43464d3ae75d5fbe9abaaad1835dc
ahmed-tahoon <77838959+ahmed-tahoon@users.noreply.github.com>
2023-12-23 12:44:47 +0200

    Update TopButtons.js

--- src/Pages/Products/Componet/TopButtons.js
+++ src/Pages/Products/Componet/TopButtons.js
@@ -13,20 +13,21 @@ const TopButtons = ({ selectedProducts, handleDeleteSelected }) => {
         <div className="flex-grow" />
         <div className="flex items-center space-x-4">
           <button
+            id="add-product-btn"
             onClick={goToAddProduct}
             className="bg-gradient-to-r from-green-500 to-green-700 text-white font-bold py-2 px-4 rounded-md hover:from-green-700 hover:to-green-900"
           >
             ADD
           </button>
-          
-            <button
-              id="delete-product-btn"
-              onClick={handleDeleteSelected}
-              className="bg-gradient-to-r from-red-500 to-red-700 text-white font-bold py-2 px-4 rounded-md hover:from-red-700 hover:to-red-900"
-            >
-              MASS DELETE
-            </button>
-          
+
+          <button
+            id="delete-product-btn"
+            onClick={handleDeleteSelected}
+            className="bg-gradient-to-r from-red-500 to-red-700 text-white font-bold py-2 px-4 rounded-md hover:from-red-700 hover:to-red-900"
+          >
+            MASS DELETE
+          </button>
+
           {selectedProducts.length > 0 && (
             <div className="flex items-center">
               <span className="text-sm text-gray-500 mr-1">Selected:</span>
